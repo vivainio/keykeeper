012e00d0e7d958bee3097f7b910bfa61d38faaf2
Ville M. Vainio <ville.vainio@basware.com>
2021-02-22 21:26:09 +0200

    create shorter param store paths by default

--- keykeeper-demo/make_jwt_authorized_request.py
+++ keykeeper-demo/make_jwt_authorized_request.py
@@ -6,9 +6,7 @@ import sys
 # pass the url to invoke
 url = sys.argv[1]
 # note that "iss" (issuer) needs to have exact match to the "issuer" bucket configured in keykeeper
-token = os.popen(
-    "keykeeper-issue --sub ville --aud all --iss https://keykeeper-jwt-issuer-eu-west-1.s3-eu-west-1.amazonaws.com"
-).read()
+token = os.popen("keykeeper-issue --sub ville --aud all").read()
 auth = "Authorization: " + token
 print(token)
 subprocess.check_call(["curl", "-H", auth, url])
--- make_jwt_authorized_request.py
+++ make_jwt_authorized_request.py
@@ -7,7 +7,7 @@ import sys
 curl_args = sys.argv[1:]
 # note that "iss" (issuer) needs to have exact match to the "issuer" bucket configured in keykeeper
 token = os.popen(
-    "keykeeper-issue --sub ville --aud all --iss https://keykeeper-jwt-issuer-eu-west-1.s3-eu-west-1.amazonaws.com"
+    "keykeeper-issue --sub ville --aud all"
 ).read()
 auth = "Authorization: " + token
 print(token)
